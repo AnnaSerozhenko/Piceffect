23310083ed98f4487d4a3acc52332e45a788ae65
Дмитрий Пыльцов <dmitrypyltsov@yandex.ru>
2018-04-24 23:07:17 +0300

    Interface for filters

---
+++ Piceffect/Interfaces/IFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Collections.Generic;
+
+namespace Piceffect
+{
+	//указатель на функцию обработки прогресса выполнения задачи
+	public delegate void ProgressDelegate(double percent);
+
+	interface IFilter
+	{
+		//получение имени обработчика
+		string HandlerName { get; }
+
+		//инициализация параметров обработчика
+		void Init(SortedList<string, object> parameters);
+
+		//установка изображения-источника
+		Bitmap Source { set; }
+
+		//получение изображения-результата
+		Bitmap Result { get; }
+
+		//Запуск обработки
+		void StartHandle(ProgressDelegate progress);
+	}
+}
