84e688a544a98de36f8d78d51ff2d559c920abdd
Юрий Капков <kapkov555@gmail.com>
2018-05-12 22:55:47 +0300

    Filters Linear and Notlinear

---
+++ Piceffect/Classes/Filters/LinearCorrection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+
+namespace Piceffect
+{
+	public class LinearCorrection : BaseFilter
+	{
+		//имя фильтра
+		public override string HandlerName => "LinearCorrection";
+
+		//внутреннее изображение
+		private Bitmap Picture;
+
+		public override Bitmap Result { get => Picture; }
+
+		//обработка
+		public override void StartHandle(ProgressDelegate progress)
+		{
+			byte min = 255, max = 0;
+			FastBitmap Image = new FastBitmap(Source);
+			Pixel pixel;
+			//long count = 0;
+			for (int y = 0; y < Image.Height; ++y)
+			{
+				for (int x = 0; x < Image.Width; ++x)
+				{
+					pixel = Image.GetPixel(x, y);
+					min = Math.Min(min, (byte)((pixel.R + pixel.G + pixel.B) / 3));
+					max = Math.Max(max, (byte)((pixel.R + pixel.G + pixel.B) / 3));
+					//progress((double)(++count) / (Source.Width * Source.Height * 2));
+				}
+			}
+			float coefficient = 255f / (max - min);
+			for (int y = 0; y < Image.Height; ++y)
+			{
+				for (int x = 0; x < Image.Width; ++x)
+				{
+					pixel = Image.GetPixel(x, y);
+					pixel.R = Limit((pixel.R - min) * coefficient);
+					pixel.G = Limit((pixel.G - min) * coefficient);
+					pixel.B = Limit((pixel.B - min) * coefficient);
+					Image.SetPixel(x, y, pixel);
+					//progress((double)(++count) / (Source.Width * Source.Height * 2));
+				}
+			}
+			Picture = Image.GetBitmap();
+		}
+	}
+}
---
+++ Piceffect/Classes/Filters/NonlinearCorrection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace Piceffect
+{
+	public class NonlinearCorrection : BaseFilter
+	{
+		//имя фильтра
+		public override string HandlerName => "NonlinearCorrection";
+
+		//внутреннее изображение
+		private Bitmap Picture;
+
+		public override Bitmap Result { get => Picture; }
+
+		//обработка
+		public override void StartHandle(ProgressDelegate progress)
+		{
+			float coefficient = 40f;
+			FastBitmap Image = new FastBitmap(Source);
+			Pixel pixel;
+			//long count = 0;
+			for (int y = 0; y < Image.Height; ++y)
+			{
+				for (int x = 0; x < Image.Width; ++x)
+				{
+					pixel = Image.GetPixel(x, y);
+                    pixel.R = Limit(coefficient * Math.Log(1 + pixel.R) / Math.Log(2));
+					pixel.G = Limit(coefficient * Math.Log(1 + pixel.G) / Math.Log(2));
+					pixel.B = Limit(coefficient * Math.Log(1 + pixel.B) / Math.Log(2));
+					Image.SetPixel(x, y, pixel);
+					//progress((double)(++count) / (Source.Width * Source.Height * 2));
+				}
+			}
+			Picture = Image.GetBitmap();
+		}
+	}
+}
