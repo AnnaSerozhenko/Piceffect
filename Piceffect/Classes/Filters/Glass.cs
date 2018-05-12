4c06ddbcd5c9446cc02f11bd723ec453852a3e14
Анна Серёженко <annieserezhenko@gmail.com>
2018-05-12 22:44:47 +0300

    Filters Glass and Noise

---
+++ Piceffect/Classes/Filters/Glass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace Piceffect
+{
+	public class Glass : BaseFilter
+	{
+		//имя фильтра
+		public override string HandlerName => "Glass";
+
+		//внутреннее изображение
+		private Bitmap Picture;
+
+		public override Bitmap Result { get => Picture; }
+
+		//обработка
+		public override void StartHandle(ProgressDelegate progress)
+		{
+			FastBitmap Image = new FastBitmap(Source);
+			Random random = new Random();
+			//long count = 0;
+			for (int y = 0; y < Image.Height; ++y)
+			{
+				for (int x = 0; x < Image.Width; ++x)
+				{
+					int newX = Math.Min(Math.Max((int)(x + random.NextDouble() * 15 - 5), 0), Image.Width - 1);
+					int newY = Math.Min(Math.Max((int)(y + random.NextDouble() * 15 - 5), 0), Image.Height - 1);
+					Image.SwapPixels(x, y, newX, newY);
+					//progress((double)(++count) / (Source.Width * Source.Height));
+				}
+			}
+			Picture = Image.GetBitmap();
+		}
+	}
+}
---
+++ Piceffect/Classes/Filters/Noise.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace Piceffect
+{
+	public class Noise : BaseFilter
+	{
+		//имя фильтра
+		public override string HandlerName => "Noise";
+
+		//внутреннее изображение
+		private Bitmap Picture;
+
+		public override Bitmap Result { get => Picture; }
+
+		//инициализация
+		public override void Init(SortedList<string, object> parameters)
+		{
+			strength = (int)parameters["strength"];
+			scale = float.Parse(parameters["scale"].ToString());
+		}
+
+		//параметры фильтра
+		int strength;
+		float scale;
+
+		//обработка
+		public override void StartHandle(ProgressDelegate progress)
+		{
+			FastBitmap Image = new FastBitmap(Source);
+			Pixel color;
+			//long count = 0;
+			Random random = new Random();
+			int size = (int)(Image.Width * Image.Height * scale);
+			for (int i = 0; i < size; ++i)
+			{
+				int x = random.Next(0, Image.Width);
+				int y = random.Next(0, Image.Height);
+				color = Image.GetPixel(x, y);
+				int shift = random.Next(-strength, strength);
+				byte r = Limit(color.R + shift);
+				byte g = Limit(color.G + shift);
+				byte b = Limit(color.B + shift);
+				Image.SetPixel(x, y, r, g, b);
+				//progress((double)(++count) / size);
+			}
+			Picture = Image.GetBitmap();
+		}
+	}
+}
